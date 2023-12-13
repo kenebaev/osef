334cfb092100e68a072b15f28ebd82c822c61d39
EnderKru <enderpro700@gmail.com>
2023-12-13 12:17:35 +0600

    oc

---
+++ os/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        item: 7,
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 2000,
+        autoplayHoverPause: true,
+    });
+});
